051d46157faa0641a952a87f3ff17af0f7965db7
jianghan0001 <jianghan0001@gmail.com>
2017-03-12 19:46:12 -0700

    1

--- session/PStest/01.py
+++ session/PStest/01.py
@@ -1,17 +1,2 @@
-print('think a number')
-high=100
-low=0
-da=False
-while True:
-    ans=(high + low)//2
-    jin=input('%d is c or h or l' %ans)
-    if jin=='c':
-        print('oh yeah!')
-        da=True
-    elif jin=='h':
-        high= ans
-    elif jin== 'l':
-        low= ans
-    else:
-        print('gundan')
-print('Your number is %d' %ans)
+
+print(2//2)
